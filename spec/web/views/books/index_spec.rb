77802b56ace0d65e28c883a13063c3e704905e7f
lokideos <archmage09@gmail.com>
2021-09-06 22:31:55 +0300

    Fix index spec

--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe Web::Views::Books::Index, type: :view do
-  let(:exposures) { Hash[format: :html] }
+  let(:exposures) { Hash[books: []] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/books/index.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #format' do
-    expect(view.format).to eq exposures.fetch(:format)
+  it 'exposes #books' do
+    expect(view.books).to eq(exposures.fetch(:books))
   end
 
   context 'when there are no books' do

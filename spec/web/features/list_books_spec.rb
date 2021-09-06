7c4e440966bee6cbf8edd736b61b64ef2f337faa
lokideos <archmage09@gmail.com>
2021-09-06 22:23:27 +0300

    Fix lists book feature spec (bad capybara css selector)

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'List books' do
     visit '/books'
 
     within '#books' do
-      expect(page).to have_css_selector('.book', count: 1), 'Expected to find 1 book'
+      expect(page).to have_selector('.book', count: 1), 'Expected to find 1 book'
       expect(page).to have_content('Practical Object-Oriented Design in Ruby')
       expect(page).to have_content('Sandi Metz')
     end
